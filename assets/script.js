b6d0807adc57a9aed9623c784d6762ab3557f5d7
unknown <anthony_rosati@yahoo.com>
2023-10-07 22:20:02 -0500

    added function for start button to disappear and be replaced by scorekeeping element, linked HTML to JS file

---
+++ assets/script.js
@@ -0,0 +1,25 @@
+// Add function to clicking "Start" button
+var startButton = document.querySelector("#start-button");
+startButton.addEventListener("click", startGame);
+
+
+// Start button function
+function startGame() {
+
+    // Removes button for game start
+    startButton.remove();
+
+    // Creates scorekeeping variables
+    var scoreBox = document.createElement("p");
+    var scoreTitle = document.createTextNode("Current Score: ");
+    var currentScore = document.createTextNode(0)
+    scoreBox.appendChild(scoreTitle);
+    scoreBox.appendChild(currentScore);
+
+    // Inserts scorekeeping element into header and assigns style 
+    var titleElement = document.querySelector("header");
+    var timer = document.querySelector("#timer")
+    titleElement.insertBefore(scoreBox, timer);
+    scoreBox.setAttribute("style", "font-size: 35px; padding: 15px 65px; position: relative; right: 10%;" )
+
+}
