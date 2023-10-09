269321817359c5ee89bb6f4dac278324610b8316
unknown <anthony_rosati@yahoo.com>
2023-10-08 20:20:04 -0500

    Added html and CSS code structure for questions

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,15 @@
 // Add function to clicking "Start" button
 var startButton = document.querySelector("#start-button");
+var highScores = document.querySelector("#high-scores")
 startButton.addEventListener("click", startGame);
 
 
 // Start button function
 function startGame() {
 
-    // Removes button for game start
+    // Removes button for game start and high scores
     startButton.remove();
+    highScores.remove();
 
     // Creates scorekeeping variables
     var scoreBox = document.createElement("p");
@@ -18,8 +20,10 @@ function startGame() {
 
     // Inserts scorekeeping element into header and assigns style 
     var titleElement = document.querySelector("header");
-    var timer = document.querySelector("#timer")
+    var timer = document.querySelector("#timer");
     titleElement.insertBefore(scoreBox, timer);
-    scoreBox.setAttribute("style", "font-size: 35px; padding: 15px 65px; position: relative; right: 10%;" )
+    scoreBox.setAttribute("style", "font-size: 35px; padding: 15px 65px; position: relative; right: 10%;" );
+
+    
 
 }
